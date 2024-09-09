a9efcea5e5333b2d2405e2442f44e715489e8ac2
EDGE775 <dima.khlapov@mail.ru>
2024-09-09 19:07:16 +0300

    4. Операторы условий if [#189221]

--- src/main/kotlin/ru/job4j/base/Calc.kt
+++ src/main/kotlin/ru/job4j/base/Calc.kt
@@ -16,6 +16,12 @@ fun divide(first: Int, second: Int): Int {
     return first / second
 }
 
+fun maxOfTwo(first: Int, second: Int) : Int
+        = if (first > second) first else second
+
+fun maxOfThree(first: Int, second: Int, third: Int) : Int
+        = if (first > second) maxOfTwo(first, third) else maxOfTwo(second, third)
+
 fun main() {
     val plus = add(1, 1)
     println("1 + 1 = $plus")
@@ -28,4 +34,10 @@ fun main() {
 
     val div = divide(3, 3)
     println("3 / 3 = $div")
+
+    val maxOfTwo = maxOfTwo(1, 2)
+    println("max from 1 and 2 is $maxOfTwo")
+
+    val maxOfThree = maxOfThree(1, 2, 3)
+    println("max from 1, 2 and 3 is $maxOfThree")
 }
--- src/test/kotlin/ru/job4j/base/CalcTest.kt
+++ src/test/kotlin/ru/job4j/base/CalcTest.kt
@@ -3,7 +3,7 @@ package ru.job4j.base
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class CalcTest {
+class CalcTest{
 
     @Test
     fun when1Plus1() {
@@ -23,5 +23,15 @@ class CalcTest {
     @Test
     fun when2Divide2() {
         assertThat(divide(2, 2)).isEqualTo(1)
+    }
+
+    @Test
+    fun whenMaxOfTwo() {
+        assertThat(maxOfTwo(3, 6)).isEqualTo(6)
+    }
+
+    @Test
+    fun whenMaxOfThree() {
+        assertThat(maxOfThree(9, 3, 6)).isEqualTo(9)
     }
 }
