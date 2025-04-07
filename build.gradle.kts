8d5f273393205dffc993ecedf66ee606bddf4cb2
EDGE775 <dima.khlapov@mail.ru>
2025-04-08 00:14:05 +0300

    Detekt | Статический анализ кода в Kotlin [#505332]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.6.0" // 1.6.0 2.0.0
+    kotlin("jvm") version "1.6.20" // JVM 17 - Kotlin 1.6.20 - Gradle 7.4.2
     checkstyle
+    id("io.gitlab.arturbosch.detekt") version "1.20.0"
 }
 checkstyle {
     configFile = rootProject.file("checkstyle.xml")
@@ -19,6 +20,21 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.24.2")
 }
 
+detekt {
+    toolVersion = "1.20.0" // Версия Detekt
+    config = files("$projectDir/config/detekt/detekt.yml") // Путь к конфигурационному файлу
+    buildUponDefaultConfig = true // Настройки будут расширять дефолтный конфиг Detekt
+    allRules = true // Использовать только правила, указанные в конфиге
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt> {
+    reports {
+        html.required.set(true) // Отчет в формате HTML
+        xml.required.set(true)  // Отчет в формате XML
+        txt.required.set(false) // Текстовый отчет
+    }
 }
