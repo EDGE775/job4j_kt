395b82a0b3282326d7d570601def8b920a333a6e
EDGE775 <dima.khlapov@mail.ru>
2025-11-04 15:04:27 +0300

    5. Apply [#338631]

---
+++ src/main/kotlin/ru/job4j/labmda/HtmlTable.kt
@@ -0,0 +1,13 @@
+package ru.job4j.labmda
+
+class HtmlTable {
+    fun table(row: Int, cell: Int): String {
+        return StringBuilder()
+            .apply {
+                append("<table>")
+                // TODO add row and cell
+                append("</table>")
+            }
+            .toString()
+    }
+}
